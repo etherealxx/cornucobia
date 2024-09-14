d161c82e635197edb34e389c1337bf39a88711d3
Etherealxx <64251396+etherealxx@users.noreply.github.com>
2024-09-15 03:45:31 +0700

    More credits stuff
    - Reduced file size for some music
    - Credit tester

--- scripts/credit_item_description.gd
+++ scripts/credit_item_description.gd
@@ -5,11 +5,20 @@ func _ready() -> void:
 
 func fill_credits_form(credit_item : CreditItem):
 	$AssetName.text = credit_item.asset_name
-	$AssetOwner.text = credit_item.asset_author
+	var owner_text = "by %s" % credit_item.asset_author
+	if !credit_item.asset_author_alt_name.is_empty():
+		owner_text += " (%s)" % credit_item.asset_author_alt_name
+	$AssetOwner.text = "by %s" % owner_text
 	$AssetType.text = "Type : %s" % credit_item.asset_type
 	$AssetUsage.text = "Usage : %s" % credit_item.asset_usage
+	if !credit_item.asset_modification.is_empty():
+		$AssetMods.show()
+		$AssetMods.text = "Modification : %s" % credit_item.asset_modification
+	else: $AssetMods.hide()
 	$AssetLicense.text = "License : %s" % credit_item.asset_license
-	if !credit_item.asset_quote.is_empty(): $AssetQuote.text = "Quote : %s" % credit_item.asset_quote
+	if !credit_item.asset_quote.is_empty():
+		$AssetQuote.show()
+		$AssetQuote.text = '"%s"' % credit_item.asset_quote
 	else: $AssetQuote.hide()
 	$AssetWebsite.text = "Website : %s" % credit_item.asset_website
 	%AssetFullLicense.text = credit_item.asset_full_license
--- scripts/tools/credit_tester.gd
+++ scripts/tools/credit_tester.gd
@@ -3,6 +3,7 @@ extends Node
 
 @export var credit_to_test : CreditItem
 @export var credit_reset_ref : CreditItem
+@export var description_node : Node
 
 @export var btn_test_credit: bool:
 	set(v): test_credit()
@@ -10,8 +11,27 @@ extends Node
 @export var btn_reset_all: bool:
 	set(v): reset_all()
 
+func fill_form(credit_item):
+	description_node.get_node("AssetName").text = credit_item.asset_name
+	var owner_text = "by %s" %credit_item.asset_author
+	if !credit_item.asset_author_alt_name.is_empty():
+		owner_text += " (%s)" % credit_item.asset_author_alt_name
+	description_node.get_node("AssetOwner").text = owner_text
+	description_node.get_node("AssetType").text = "Type : %s" % credit_item.asset_type
+	description_node.get_node("AssetUsage").text = "Usage : %s" % credit_item.asset_usage
+	description_node.get_node("AssetMods").text = "Modification : %s" % credit_item.asset_modification
+	description_node.get_node("AssetLicense").text = "License : %s" % credit_item.asset_license
+	if !credit_item.asset_quote.is_empty():
+		description_node.get_node("AssetQuote").show()
+		description_node.get_node("AssetQuote").text = '"%s"' % credit_item.asset_quote
+	else: description_node.get_node("AssetQuote").hide()
+	description_node.get_node("AssetWebsite").text = "Website : %s" % credit_item.asset_website
+	description_node.get_node("FullLicenseCont/AssetFullLicense").text = credit_item.asset_full_license
+
 func test_credit():
-	%CreditItemDescription.fill_credits_form(credit_to_test)
+	fill_form(credit_to_test)
+	description_node.get_node("FullLicenseCont").show()
 	
 func reset_all():
-	%CreditItemDescription.fill_credits_form(credit_reset_ref)
+	fill_form(credit_reset_ref)
+	description_node.get_node("FullLicenseCont").hide()
